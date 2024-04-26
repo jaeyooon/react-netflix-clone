d5465dcb4e5277693ee206d97e4e60ad3cd7e12a
jaeyooon <jycho0208@gmail.com>
2024-04-27 00:12:21 +0900

    슬라이드 기능 추가 & styled component 이용한 Footer 생성 & 영화 자세히 보기 모달 생성

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,10 +1,13 @@
 import axios from '../api/axios';
 import React, { useEffect, useState } from 'react'
 import "./Row.css";
+import MovieModal from './MovieModal';
 
 export default function Row({ title, fetchUrl, isLargeRow, id }) {
 
   const [movies, setMovies] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [movieSelected, setMovieSelected] = useState({});
 
   useEffect(() => {
     fetchMovieData();
@@ -16,12 +19,22 @@ export default function Row({ title, fetchUrl, isLargeRow, id }) {
     setMovies(request.data.results);    // 영화 정보들을 movies state에 넣어줌.
   }
 
+  const handleClick = (movie) => {    // 클릭한 movie 정보를 가져옴
+    setModalOpen(true);
+    setMovieSelected(movie);    // movie 정보를 넣어줌
+  }
+
   return (
     <section className='row'>
       <h2>{title}</h2>
       <div className='slider'>
         <div className='slider__arrow-left'>
-          <span className='arrow'>{"<"}</span>
+          <span className='arrow'
+            onClick={() => {
+              document.getElementById(id).scrollLeft -= window.innerWidth - 80;
+            }}>
+            {"<"}
+          </span>
         </div>
         <div id={id} className='row__posters'>
           {movies.map(movie => (
@@ -30,13 +43,24 @@ export default function Row({ title, fetchUrl, isLargeRow, id }) {
               className={`row__poster ${isLargeRow && "row__posterLarge"}`}
               src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
               alt={movie.name}
+              onClick={() => handleClick(movie)}
             />
           ))}
         </div>
         <div className='slider__arrow-right'>
-          <span className='arrow'>{">"}</span>
+          <span className='arrow'
+            onClick={() => {
+              document.getElementById(id).scrollLeft += window.innerWidth - 80;
+            }}>
+            {">"}
+          </span>
         </div>
       </div>
+      {
+        modalOpen && (
+          <MovieModal {...movieSelected} setModalOpen={setModalOpen} />   // props로 movie 정보 넣어줌.
+        )
+      }
     </section>
   )
 }
